bfefdcdc545faaa646afc05dc82e2c30822f8c0c
Shannon Appelcline <shannon.appelcline@gmail.com>
2020-09-02 14:01:43 -1000

    Create 17_3_getinfo.js

---
+++ src/17_3_getinfo.js
@@ -0,0 +1,15 @@
+const RpcAgent = require('bcrpc');
+agent = new RpcAgent({port: 18332, user: 'StandUp', pass: '6305f1b2dbb3bc5a16cd0f4aac7e1eba'});
+
+agent.getBlockCount(function (err, blockCount) {
+  if (err)
+    throw Error(JSON.stringify(err));
+  console.log(blockCount.result);
+
+  agent.getBlockHash(blockCount.result, function (err, hash) {
+    if (err)
+      throw Error(JSON.stringify(err));
+    console.log(hash.result);
+  })
+
+});
